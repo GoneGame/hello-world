9f2ea8cffd05faf8e48e9abc2b76a7634707c55a
GoneGame <doom_lol97@outlook.com>
2016-10-15 22:37:16 +0800

    Add files via upload

---
+++ test/1_/Hello World.c
@@ -0,0 +1,20 @@
+/* Kiren Mohan */
+#include <stdio.h>
+
+main()
+{
+	char key;
+	
+	printf("Hello World!\n");
+	printf("The beginning...\n");
+	printf("Press any key to end\n");
+	
+	scanf("%c", &key);
+	
+	printf("The key you pressed was %c:", key);
+	
+	return 0;
+} 
+
+/*The pogram ends here*/
+
